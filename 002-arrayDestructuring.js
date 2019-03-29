96820b013baf97814a8a1b3ae25ac197a53ff9da
UchuNoKen <xen.o.fite@gmail.com>
2019-03-29 17:51:09 -0400

    basic destructuring

---
+++ 002-arrayDestructuring.js
@@ -0,0 +1,10 @@
+// Array Destructuring ----------------------------------------------------
+
+const rgb = [255, 200, 0];
+
+// *!* Using basic array destructuring:
+//       - use an array literal on the left-hand side of an assignment expression
+
+const [red, green, blue] = rgb;
+
+console.log(`R: ${red}, G: ${green}, B: ${blue}`); // R: 255, G: 200, B: 0
