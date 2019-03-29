ff6cd4431614d99a4e853e78ea50289320c41402
UchuNoKen <xen.o.fite@gmail.com>
2019-03-29 17:09:21 -0400

    variable assignment

--- block-001.js
+++ block-001.js
@@ -26,3 +26,26 @@ const student = {
 
 // Using basic object destructuring:
 //  - use an object literal on the left-hand side of an assignment expression
+
+const { firstname, lastname, country } = student;
+console.log(firstname, lastname, country); // "Joe", "Schmoe", "USA"
+
+// Values were assigned to three variables using the value from their corresponding
+// object keys on the 'student' object.
+// Variables are created if they do not exist
+
+// Using destructuring in variable assignment:
+//  - enclose in parentheses since it is an assignment expression
+
+let country = "Brazil";
+let firstname = "Kent";
+let lastname = "Clark";
+
+const student = {
+  firstname: "Vlad",
+  lastname: "Empaluer",
+  country: "Bromania"
+};
+
+// reassign firstname and lastname using destructuring
+({ firstname, lastname } = student);
