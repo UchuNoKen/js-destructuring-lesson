dd8abee86dbb0a163c4cd7c4711efca6b6561366
UchuNoKen <xen.o.fite@gmail.com>
2019-03-29 17:00:27 -0400

    add object

--- block-001.js
+++ block-001.js
@@ -9,3 +9,20 @@
 
 // Problem...
 //      We need to display some information based on this data
+
+// Object Destructuring ----------------------------------------------------
+
+const student = {
+  firstname: "Joe",
+  lastname: "Schmoe",
+  age: 31,
+  country: "USA",
+  scores: {
+    maths: 99,
+    english: 100,
+    science: 98
+  }
+};
+
+// Using basic object destructuring:
+//  - use an object literal on the left-hand side of an assignment expression
