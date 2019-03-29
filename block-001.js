0b8569fc2d64924d7a7e64d0be54e3b3a8f32d0c
UchuNoKen <xen.o.fite@gmail.com>
2019-03-29 16:52:19 -0400

    destructing block 1

---
+++ block-001.js
@@ -0,0 +1,11 @@
+// Why Destructuring? --------------------------------------------------
+
+// Domain...
+//      The data of a student including scores in three subject is represented in
+//      an object
+//          - Math score
+//          - English score
+//          - Science score
+
+// Problem...
+//      We need to display some information based on this data
